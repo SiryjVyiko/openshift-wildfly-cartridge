279a781a7284ee545f3763407cad9b41b569409a
SiryjVyiko <dz@hivext.net>
2015-02-04 11:46:12 +0200

    Update firewall.sh

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -3,4 +3,4 @@
 grep -q '4949.*REDIRECT.*ports.*9990' /etc/sysconfig/iptables && return 0;
 /sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990;
 grep -q '4848.*REDIRECT.*ports.*9993' /etc/sysconfig/iptables && return 0;
-/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990;
+/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993;
