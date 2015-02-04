7691acc02dde36d38589cbf23e53711f894b7079
SiryjVyiko <dz@hivext.net>
2015-02-04 09:21:11 +0200

    Update firewall.sh

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
 
-/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990
-/sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993
+local target_chain_http=`iptables -t nat -L -n | grep .*4949.*redir.*ports.*9990.*`;
+[ -z "target_chain_http" ] && { /sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990; }
+local target_chain_https=`iptables -t nat -L -n | grep .*4848.*redir.*ports.*9993.*`;
+[ -z "target_chain_https" ] && { /sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4848 -j REDIRECT --to-ports 9993; }
